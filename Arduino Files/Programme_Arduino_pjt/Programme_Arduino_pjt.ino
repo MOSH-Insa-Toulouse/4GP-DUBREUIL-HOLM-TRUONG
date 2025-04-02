af736342d15fe67693bba9ca2b00a3bbc0cbd7d3
Pierrick DUBREUIL <pierrick_dubreuil@Host-002.lan>
2025-04-02 16:57:19 +0200

    Ajout de la fonction de lecture du capteur dans l'arduino + Suppression de fichiers doublons

--- Arduino Files/Programme_Arduino_pjt/Programme_Arduino_pjt.ino
+++ Arduino Files/Programme_Arduino_pjt/Programme_Arduino_pjt.ino
@@ -20,17 +20,17 @@ Il faut encore ajouter les modules suivants :
   - Le flex-sensor
 """
 #define EncA 2 // Pin CLK encodeur
-#define EncB 4 // Pin DT encordeur
+#define EncB 4 // Pin DT encodeur
 #define Switch 5 // Switch connexion
 #define rxPin 8// Broche utilisée pour la lecture des données BT
 #define txPin 7// Broche utilisée pour l'écriture des données BT
-
+#define PinServo 12 // Pin de commande servomoteur
 #define PixLength  128 // Taille de OLED en longueur
 #define PixWidth  64 // Taille de OLED en Largeur
 #define brocheResetOLED  -1 // Broche reset OLED
 #define AdressI2COLED 0x3C // Adresse Protocole O2C OLED
 #define baudrate 9600
-
+#define PinCapteur A0 // Pin Capteur Graphene
 
 """BT Mod"""
 SoftwareSerial mySerial(rxPin, txPin);
@@ -44,7 +44,10 @@ Adafruit_SSD1306 OLED(PixLength, PixWidth, &Wire, brocheResetOLED);
 
 """Servo-moteur Mod"""
 Servo myServo;
-init pos = 0;
+int pos = 0;
+int pos_max = 150;
+int pos_min = 20;
+
 
 
 
@@ -52,10 +55,8 @@ init pos = 0;
 
 
 void setup() {
-  // put your setup code here, to run once:
 
-
-"""BT Mod"""
+  """BT Mod"""
 pinMode(rxPin, INPUT);
 pinMode (txPin, OUTPUT);
 mySerial.begin(baudrate);
@@ -63,12 +64,12 @@ Serial.begin(baudrate);
 Serial.println("start")
 
 """Encodeur Mod"""
-
 pinMode(EncA, INPUT);
 digitalWrite(EncA, HIGH)
 pinMode(EncB, INPUT);
 digitalWrite(EncB, HIGH)
 attachInterrupt(0, doEncoder, RISING);
+
 """OLED Screen Mod"""
 if(!OLED.begin(SSD1306_SWITCHCAPVCC, AdressI2COLED))
 while(1);
@@ -99,9 +100,13 @@ for(byte tailleCaractere = 1; tailleCaractere <=3, tailleCaractere++){
 }
 
 """Servo-moteur Mod"""
-myServo.attach(12); // Pin connectée pour le servo-moteur
+myServo.attach(PinServo); // Pin connectée pour le servo-moteur
 }
-
+void measure(){
+  """Fonction qui fait la lecture analogique de la tension produite par le capteur graphene"""
+  Serial.println(analogRead(PinCapteur));
+  delay(50);
+}
 
 
 
@@ -128,13 +133,19 @@ while(Serial.avaible()){
 Serial.print("Position : ");
 Serial.println(Enc0Pos, DEC)
 
-"""Servo Mod"""
-for(pos = 0; pos<180; pos+=1){
+"""Servo Mod
+Les deux premières boucle for font le calcul pour le capteur graphene
+"""
+
+
+for(pos = pos_min; pos<pos_max; pos+=1){
   myServo.write(pos);
+  measure();
   delay(15);
 }
-for(pos = 180;pos>=1; pos-= 1){
+for(pos = pos_max;pos>=pos_min; pos-= 1){
   myServo.write(pos);
+  measure();
   delay(15);
 }
 }
@@ -149,3 +160,4 @@ void doEncoder(){
     Enc0Pos--;
   }
 }
+
